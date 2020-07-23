055955d2b6a6d3a6fe0c3cb1976d058be05f6731
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2020-07-23 21:27:37 +0800

    Delete multiple10.py

--- _99_py_SOX_pkg_v0.0.3 -git/multiple10.py
+++
@@ -1,9 +0,0 @@
-from single import single_meas
-
-if __name__ == "__main__":
-    #multiple times
-    meas_N_times = 10
-    i = 0
-    while i < meas_N_times:
-        single_meas()
-        i += 1
