f8dc3a836a3ab7de8217dda185c077dbffefbcd7
ZL <34211023+AuroraBoreas@users.noreply.github.com>
2022-05-09 08:11:45 +0800

    Add files via upload

--- 20210830 TV_Pmod_Mura_Correction_Checker/main.py
+++ 20210830 TV_Pmod_Mura_Correction_Checker/main.py
@@ -1,49 +1,29 @@
-import sys, os
-sys.path.append('.')
-
-from library.excel.image import MuraImageLoader, logging, Path
-from library.plrLog import SetLogConverter
-from library.extractor import Extractor
-import datetime
-
-def clean_dst_history(dstBinFolder:Path, dstImageFolder:Path):
-    for root, _, files in os.walk(dstBinFolder):
-        for file in files:
-            os.remove(os.path.join(root, file))
-            
-    for root, _, files in os.walk(dstImageFolder):
-        for file in files:
-            os.remove(os.path.join(root, file))
+from library.checker import (datetime, logging, MuraChecker, srcBinFolder)
 
 def main():
-    # srcBinFolder   = r'\\43.98.232.61\电气\XU_J\AG65_MURA'
-    pmod_name      = "AG65_ITC(CHN)"
-    # dstBinFolder   = "data"
-    # dstImageFolder = "Images"
-
-    # clean_dst_history(dstBinFolder, dstImageFolder)
-    # e1 = Extractor(srcBinFolder, dstBinFolder)
-    # e1.copy()
-
-    # plr_exe        = "PLRLog.exe"
-    # dstBinFolder   = "data"
-    # img_path       = "img.bmp"
-    # dstImageFolder = "Images"
-    
-    # slc = SetLogConverter(plr_exe, dstBinFolder, img_path, dstImageFolder)
-    # slc.to_image()
-
+    pmod_name      = "FW75" # pmod name
+    dstBinFolder   = "data"
+    dstImgFolder   = "Images"
+    plr_exe        = "PLRLog.exe"
+    img_path       = "img.bmp"
+    ire            = 640 # 128, 384, 640, 896, 1024
     template       = r'templates\Mura_List_Template.xlsx'
-    dst_xl         = '{} {}_Mura_Image_List.xlsx'.format(datetime.date.today().strftime('%Y%m%d'), pmod_name)
+    dst_xl         = '{}_{}_Mura_Image_List_{}.xlsx'.format(datetime.datetime.today().strftime('%Y%m%d%H%M%S'), pmod_name, ire)
     src_img_folder = 'Images'
-    dstWidth      = 240
-    dstCellName   = 'B'
-    dstCellCol384 = 'C'
-    dstCellCol640 = 'D'
-    mil = MuraImageLoader(template, dst_xl, src_img_folder, dstWidth, dstCellName, dstCellCol384, dstCellCol640)
-    mil.work()
-    mil.clean()
-    logging.info("Mura Images loaded into Excel successfully!")
+    dstWidth       = 240
+    dstCellName    = 'B'
+    dstCellColO    = 'C'
+    dstCellColR    = 'D'
+    interval       = 1 # sampling
+
+    logging.info("starting..")
+    mura_checker = MuraChecker()
+    mura_checker.clean_hist(dstBinFolder, dstImgFolder)
+    mura_checker.extract_src(srcBinFolder, dstBinFolder, interval, ire)
+    mura_checker.convert_img(plr_exe, dstBinFolder, img_path, dstImgFolder)
+    m = mura_checker.make_report(template, dst_xl, src_img_folder, dstWidth, dstCellName, dstCellColO, dstCellColR, ire)
+    m.work(m.cmp_both)
+    logging.info('Done')
 
 if __name__ == '__main__':
     main()
